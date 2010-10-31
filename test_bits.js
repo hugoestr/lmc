7ee93124bf5432cbdc5bca737c48e1e3dccb884c
Hugo Estrada <hugoestr@cal.berkeley.edu>
2010-10-30 22:17:57 -0400

    Default value defaults to zero

--- test_bits.js
+++ test_bits.js
@@ -8,6 +8,10 @@ test("Bit has base defaults to 2", function(){
     equals(b.base, 2, "default base is 2");
 });
 
+test("Bit has a value, defaults to zero,", function(){
+  equal(b.value, 0, "It should default to zero");
+});
+
 test("Bit be AND", function(){
     
 });
