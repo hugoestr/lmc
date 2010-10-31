fd42024ea21be2dc853ddac9e18de804c02e16a5
Hugo Estrada <hugoestr@cal.berkeley.edu>
2010-10-30 23:25:13 -0400

    Bit adds value. Bit Adds value in base 8.

--- Bit/test_bits.js
+++ Bit/test_bits.js
@@ -16,26 +16,26 @@ test("It should default to base 2", function(){
 
 test("It should default value to zero,", function(){
    SetUp();
-   equal(b.value, 0, "It should default to zero");
+   equals(b.value, 0, "It should default to zero");
 });
 
 test("It should add to value", function(){
     SetUp();
     b.Add(1);
-    equal(b.value, 1, "Value should be 1");
+    equals(b.value, 1, "Value should be 1");
 });
 
 test("It should transforms greater values than its base and add the correct bit", function(){
   SetUp();
   b.Add(2);
-  equal(b.value, 0 , "Value should be 0 because 00 & 10 return 10, with this value being 0" );
+  equals(b.value, 0 , "Value should be 0 because 00 & 10 return 10, with this value being 0" );
 });
 
 test("It should substract a value", function(){
   SetUp();
   b.value = 1;
   b.Subtract(1);
-  equal(b.value, 0 , "Value should be 0");
+  equals(b.value, 0 , "Value should be 0");
 });
 
 test("It should determine its bit value, subtract that value when value greater than its bit value.", function(){
@@ -47,13 +47,13 @@ test("It should determine its bit value, subtract that value when value greater 
 
 test("It should default its carry value to zero", function(){
   SetUp();
-  equal(b.carry, 0, "Carry value defaults to zero");
+  equals(b.carry, 0, "Carry value defaults to zero");
 });
 
 test("It should set carry value when addition exceeds base", function(){
   SetUp();
   b.Add(2);
-  equal(b.carry, 1, "carry is set to 1");
+  equals(b.carry, 1, "carry is set to 1");
 });
 
 test("It should set carry for subtraction if value exceeds base", function(){
@@ -72,5 +72,11 @@ function SetUpBase8()
 
 test("It should set default base from constructor", function(){
   SetUpBase8();
-  equal(b.base, 8, "It should default to 8");
+  equals(b.base, 8, "It should default to 8");
+});
+
+test("It should add in base 8", function(){
+  SetUpBase8();
+  b.Add(7);
+  equals(b.value, 7, "Adding 7 in base 8");
 });
