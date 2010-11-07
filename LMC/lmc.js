7492991f60b55e0f80d4418d7926e9ae4bf27ec8
Hugo Estrada <hugoestr@cal.berkeley.edu>
2010-11-07 01:12:42 -0400

    The Lmc now has an accumlator; initialize_memory takes now byte_size, base, and array_size as arguments.

--- LMC/lmc.js
+++ LMC/lmc.js
@@ -1,13 +1,17 @@
 function Lmc(){
-  this.memory = this.initialize_memory();
+  var byte_size = 3;
+  var base = 10; 
+
+  this.accumulator = new Bytes(byte_size, base);
+  this.memory = this.initialize_memory(byte_size, base, 100);
 }
 
-Lmc.prototype.initialize_memory = function(){
+Lmc.prototype.initialize_memory = function(byte_size, base, array_size){
   var size = 100;
   var result = [];
-  for(var i = 0; i < 100; i++)
+  for(var i = 0; i < array_size; i++)
   {
-    result.push(new Bytes(3, 10));
+    result.push(new Bytes(byte_size, base));
   }
 
   return result;
--- LMC/test_lmc.js
+++ LMC/test_lmc.js
@@ -12,3 +12,8 @@ test("It has memory", function(){
   SetUp();
   equals(l.memory.length, 100, "It has cells");
 });
+
+test("It has an accumulator that defaults in 0", function(){
+  SetUp();
+  equals(l.accumulator.value, 0, "Accumulator defaults in 0");
+});
